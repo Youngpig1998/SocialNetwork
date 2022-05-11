e9f4b44301af8fd50d25eb7a1f248890630fa953
Youngpig1998 <873185434@qq.com>
2022-05-11 14:34:09 +0800

    update

--- deploy/deployCompute.sh
+++ deploy/deployCompute.sh
@@ -1,12 +1,12 @@
 #deploy logic services
-kubectl apply -f ./SocialNetInComputeFork8s/url-shorten-service/.
-kubectl apply -f ./SocialNetInComputeFork8s/user-service/.
-kubectl apply -f ./SocialNetInComputeFork8s/user-mention-service/.
+kubectl apply -f ./SocialNetInComputeFork8s/service/url-shorten-service/.
+kubectl apply -f ./SocialNetInComputeFork8s/service/user-service/.
+kubectl apply -f ./SocialNetInComputeFork8s/service/user-mention-service/.
 
 sleep 10s
 
-kubectl apply -f ./SocialNetInComputeFork8s/text-service/.
-kubectl apply -f ./SocialNetInComputeFork8s/unique-id-service/.
+kubectl apply -f ./SocialNetInComputeFork8s/service/text-service/.
+kubectl apply -f ./SocialNetInComputeFork8s/service/unique-id-service/.
 #kubectl apply -f ./SocialNetInComputeFork8s/media-filter-service/.
-kubectl apply -f ./SocialNetInComputeFork8s/text-filter-service/.
+kubectl apply -f ./SocialNetInComputeFork8s/service/text-filter-service/.
 #kubectl apply -f ./SocialNetInComputeFork8s/nginx-thrift/.
